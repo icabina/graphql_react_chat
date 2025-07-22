875c0c6a743176ca9d3b026a7577422d13698218
Jorge Roldan <jorge.roldan@konnectway.com>
2025-07-22 10:22:28 -0500

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import Products from "./components/Products";
+// import Products from "./components/Products";
 import Chat from "./components/Chat";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Products</h1>
-      <Products />
+      <h1>Chat</h1>
+      <Chat />
     </div>
   );
 }
